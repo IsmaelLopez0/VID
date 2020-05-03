2dffc262be4a391cf2447e407e5fe58781a9547c
Camberitos <60439292+Camberitos@users.noreply.github.com>
2020-05-03 09:29:44 -0600

    Add files via upload

--- VID/recetario/urls.py
+++ VID/recetario/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.conf.urls import url, include
+from django.contrib import admin
 from .views import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('busqueda/', resBusqueda, name='resBusqueda'),
+    path('registro/', registro, name='registro')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
